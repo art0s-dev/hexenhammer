c5c1dd856d2368f9ce0d8d5400372a0a415c9fc3
Till Kaiser <art0s@mailbox.org>
2023-11-02 00:27:11 +0100

    feature(unit): simplified api and did more documentation

--- src/main/java/core/entitys/Unit.java
+++ src/main/java/core/entitys/Unit.java
@@ -2,48 +2,83 @@ package core.entitys;
 
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-
-import core.entitys.Weapon.SpecialRuleWeapon;
 
 /**
  * This is the cornerstone of the software
  * Each unit represents a bunch of weapons that will be rolled
- * against another unit 
+ * against another unit   
  */
 public class Unit {
 	
-	private int index = 0;
-	public record Equipment(Weapon weapon, int quantity) {}; 
-	private HashMap<Integer, Equipment> weapons = 
-			new HashMap<Integer, Equipment>();
-	
-	private HashSet<SpecialRuleUnit> specialRules = new HashSet<SpecialRuleUnit>();
-	public enum SpecialRuleUnit{
-		REROLL_ONES_TO_HIT
+	/**
+	 * With this method we add, delete and edit the weapons of a unit.
+	 * When the weapon is already there it depends on the quantity if it is deleted
+	 * For example if we say bolter subtract 5 and there are only 4 bolters, they get deleted
+	 * if we set bolters to 0 we will also delete the entry 
+	 * When the quantity is positive then the entry will be set to the given value
+	 * if there is no enty, we add it 
+	 */
+	public void change(Weapon weapon, int quantity) {
+		var weaponDoesNotExist = !weapons.contains(weapon);
+		if(weaponDoesNotExist) {
+			weapons.add(weapon);
+			quantities.put(weapon, quantity);
+			return;
+		}
+		
+		var quantityWillBeNegative =  quantities.get(weapon) + quantity <= 0;
+		if(quantityWillBeNegative || quantity == 0) {
+			weapons.remove(weapon);
+			quantities.remove(weapon);
+			return;
+		}
+		
+		quantities.put(weapon, quantity);
 	}
 	
 	/**
+	 * The SpecialRuleUnit builds an api for editing special cases in our unit
+	 * these rules apply to the battle sequence in global. There is also
+	 * a SpecialRuleWeapon which will be applied before the global rule if it is better
+	 */
+	public enum SpecialRuleUnit{
+		REROLL_ONES_TO_HIT
+	}
+	public void add(SpecialRuleUnit specialRule) {
+		this.specialRules.add(specialRule);
+	}
+	public void remove(SpecialRuleUnit specialRule) {
+		this.specialRules.remove(specialRule);
+	}
+
+	/**
 	 * The attack method is an implementation of the combat sequence of the 
-	 * warhammer 40k rulebookit should cover the shooting phase, 
+	 * warhammer 40k rulebookit should cover the shooting phase,  
 	 * aswell as the fight phase and all the generic special rules
-	 * https://www.warhammer-community.com/2023/06/02/download-the-new-warhammer-40000-rules-for-free-right-here/
+	 * https://www.warhammer-community.com/2023/06/02/download-the-new-warhammer-40000-rules-for-free-right-here/ 
 	 */
 	public double attack(Profile enemy) {
 		double damage = 0;
 		
-		for (Map.Entry<Integer, Equipment> equipment : weapons.entrySet()) {
-			var weapon = equipment.getValue().weapon();
-			var quantity = equipment.getValue().quantity();
-			
-			//Calculate the hits 
-			var numberOfShots = weapon.getAttacks() * quantity;
-			var hits = (double) numberOfShots  * weapon.getToHit();
+		for (Weapon weapon : weapons) {
+			/**
+			 * calculate the hits
+			 * we determine the number of attacks the unit has and let them roll to hit
+			 * here we can apply modifiers like sustained hits
+			 * and after that we apply rerolls 
+			 */
+			double attacks = weapon.getAttacks() * quantities.get(weapon);
+			double hits = (double) attacks  * weapon.getToHit();
 			if(this.has(SpecialRuleUnit.REROLL_ONES_TO_HIT)) {
-				hits += ((numberOfShots - hits) / 6) * weapon.getToHit(); 
+				hits += ((attacks - hits) / 6) * weapon.getToHit(); 
 			}
 			
-			//Calculate the wounds   
+			/**
+			 * calculate the wounds
+			 * in this section we compare the attacking weapon and the defending
+			 * profile against each other to determine a probability to wound the target. 
+			 * The default case is the worst case (so it only can get better)
+			 */
 			int strength = weapon.getStrength();
 			int toughness = enemy.getToughness();
 			double probabilityToWound = Probability.SIX_UP;
@@ -51,9 +86,17 @@ public class Unit {
 			if(strength > toughness) { probabilityToWound = Probability.THREE_UP;}
 			if(strength == toughness) { probabilityToWound = Probability.FOUR_UP;}
 			if(strength < toughness) { probabilityToWound = Probability.FIVE_UP; }
-			var wounds = hits * probabilityToWound;
+			double wounds = hits * probabilityToWound;
 			
-			//Calculate the saves  
+			/**
+			 * calculate the saves
+			 * here we calculate how high the chances are for a unit to tank the hit
+			 * we have 2 different mechanics: we tank with the armor or we
+			 * dodge the attack. The armor save can be modified via armor penetration
+			 * the invulnerable(dodge)-save cannot be modified. If its higher, we 
+			 * take the dodge save. For determining the probability to tank a hit we assign
+			 * an int value and subtract the armor pen.
+			 */
 			double armourSave = enemy.getArmorSave();
 			HashMap<Double,Integer> symbolicArmourSave = new HashMap<>();
 			symbolicArmourSave.put(Probability.SIX_UP, 1);
@@ -67,35 +110,34 @@ public class Unit {
 				probabilityToSave = enemy.getInvulnerableSave();
 			} 
 			
-			//calculate Damage
+			/**
+			 * calculate damage
+			 * during this step we determine how the damage is done to a model.
+			 * we substract the tanked or dodged wounds from the wound pool 
+			 * and apply the damage for each wound. Each wound can be assigned to a new
+			 * model in the unit. that means if a weapon has damage 3 and a model
+			 * has only 2 hp, the weapon can only deal 2 hp damage.
+			 * We round the number of wounds down to have an integer of damage because 
+			 * there are no 12.56555 hitpoints on a unit.
+			 * Also we apply further wound migitation mechanics like feel no pain
+			 */
 			double missedSaves = Math.floor(wounds - (wounds * probabilityToSave));
-			double damageMultiplier = weapon.getDamage() > enemy.getHitPoints() ?
-					enemy.getHitPoints() : weapon.getDamage();
+			double damageMultiplier = weapon.getDamage();
+			if(weapon.getDamage() > enemy.getHitPoints()) {
+				damageMultiplier = enemy.getHitPoints();
+			}
 			double woundsAfterFeelNoPain = (missedSaves * damageMultiplier) * enemy.getFeelNoPain();
 			damage += (missedSaves * damageMultiplier) - woundsAfterFeelNoPain;
 		}
 		
 		return damage;
 	}
-
-	public int add(Equipment equipment) {
-		int savedIndex = index;
-		weapons.put(index, equipment);
-		index++;
-		return savedIndex;
-	}
-	public void remove(int key) {
-		weapons.remove(key);
-	}
 	
-	public void add(SpecialRuleUnit specialRule) {
-		this.specialRules.add(specialRule);
-	}
-	public void remove(SpecialRuleUnit specialRule) {
-		this.specialRules.remove(specialRule);
-	}
+	private HashSet<Weapon> weapons = new HashSet<Weapon>();
+	private HashMap<Weapon, Integer> quantities = new HashMap<>();
 	private boolean has(SpecialRuleUnit specialRule) {
 		return this.specialRules.contains(specialRule);
 	}
-	
+	private HashSet<SpecialRuleUnit> specialRules = new HashSet<SpecialRuleUnit>();
+
 }
--- src/test/java/unittests/UnitTest.java
+++ src/test/java/unittests/UnitTest.java
@@ -1,20 +1,106 @@
 package unittests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import java.util.HashMap;
 
 import org.junit.jupiter.api.Test;
 
 import core.entitys.Probability;
 import core.entitys.Profile;
 import core.entitys.Unit;
-import core.entitys.Unit.Equipment;
 import core.entitys.Unit.SpecialRuleUnit;
 import core.entitys.Weapon;
 
 class UnitTest {
+	
+	/**
+	 * This Test shall just test the simple combat mechanic
+	 * of hit, wound and damage
+	 */
+	@Test
+	void spaceMarines_against_guardsmen() {
+		var spaceMarines = new Unit();
+		spaceMarines.change(bolter, 5);
+		double damage = spaceMarines.attack(guardsmen);
+		//10 Attacks with 3+ to hit, 3+ to wound and a 5up save
+		double hits = 10 * Probability.THREE_UP;
+		double wounds = hits * Probability.THREE_UP;
+		double expectedDamage = Math.floor(wounds - (wounds * Probability.FIVE_UP)); 
+		
+		assertEquals(expectedDamage, damage);
+	}
+	
+	/**
+	 * Here we shall test, if the dodge save of our rangers will be taken
+	 * instead of the armor save
+	 */
+	@Test
+	void spaceMarines_against_eldarRangers() {	
+		var spaceMarines = new Unit();
+		spaceMarines.change(heavyBolter, 4);
+		double damage = spaceMarines.attack(eldarRangers);
+		double hits = 12 * Probability.THREE_UP;
+		double wounds = hits * Probability.THREE_UP;
+		double missedSaves = Math.floor(wounds - (wounds * Probability.FIVE_UP)); 
+		double expectedDamage = missedSaves * eldarRangers.getHitPoints();
+		
+		assertEquals(expectedDamage, damage);
+	}
+	
+	/**
+	 * Here we test the damage - we expect the maximum number 
+	 * of damage to be equal to the number of hitpoints * missed Saves
+	 */
+	@Test
+	void spaceMarines_against_other_spaceMarines() {
+		var spaceMarines = new Unit();
+		spaceMarines.change(heavyBolter, 4);
+		double damage = spaceMarines.attack(otherSpaceMarines);
+		double hits = 12 * Probability.THREE_UP;
+		double wounds = hits * Probability.THREE_UP;
+		double failedSaves = wounds - (wounds * Probability.FIVE_UP); 
+		int hitPoints = otherSpaceMarines.getHitPoints();
+		double expectedDamage = Math.floor(failedSaves) * hitPoints;
+		
+		assertEquals(expectedDamage, damage);
+	}
+	
+	/**
+	 * On this one we test the wound migitation of feel no pain.
+	 * we want to halve the incoming damage
+	 */
+	@Test
+	void spaceMarines_verus_abberants() {
+		var spaceMarines = new Unit();
+		spaceMarines.change(bolter, 5);
+		double damage = spaceMarines.attack(abberants);
+		double hits = 10 * Probability.THREE_UP;
+		double wounds = hits * Probability.FIVE_UP;
+		double missedSaves = Math.floor(wounds - (wounds * Probability.FIVE_UP));
+		//Feel no pain wound migitation
+		double expectedDamage = missedSaves * Probability.FOUR_UP;
+		
+		assertEquals(expectedDamage, damage);
+	}
+	
+	/**
+	 * The Space marines have braught reinforcements!
+	 * A special rule allows our models to reroll ones on the hit roll
+	 */
+	@Test
+	void SpaceMarinesWithACaptian_AttackGuardsmen_RerollOnesToHit() {
+		var spaceMarines = new Unit();
+		spaceMarines.add(SpecialRuleUnit.REROLL_ONES_TO_HIT);
+		spaceMarines.change(bolter, 5);
+		var damage = spaceMarines.attack(guardsmen);
+		var hits = 10 * Probability.THREE_UP;
+		var missedHits = 10 - hits;
+		//We reroll only ones
+		hits += (missedHits / 6) * Probability.THREE_UP;
+		var wounds = hits * Probability.THREE_UP;
+		var expectedDamage = Math.floor(wounds - (wounds * Probability.FIVE_UP));
+		
+		assertEquals(expectedDamage, damage);
+	}
 	
 	Weapon bolter = Weapon.builder()
 			.attacks(2)
@@ -43,151 +129,16 @@ class UnitTest {
 			.invulnerableSave(Probability.FIVE_UP)
 			.build();
 	
+	Profile otherSpaceMarines = Profile.builder()
+			.toughness(4)
+			.armorSave(Probability.THREE_UP)
+			.hitPoints(2)
+			.build();
 	
-	private Equipment testEquipment = new Equipment(bolter, 1);
-
-	@Test
-	void WhenUnitWithNoWeaponAttacks_ItMakesNoDamage() {
-		var unit = new Unit();
-		double damage = unit.attack(guardsmen);
-		assertEquals(damage, 0.00);
-	}
-	
-	@Test
-	void modelJoinsAUnit() {
-		var unit = new Unit();
-		unit.add(testEquipment);
-		assertTrue(getWeapons(unit).size() > 0);
-	} 
-	
-	@Test
-	void modelLeavesAUnit() {
-		var unit = new Unit();
-		int key = unit.add(testEquipment);
-		unit.remove(key);
-		assertEquals(getWeapons(unit).size(), 0);
-	}
-	
-	@Test
-	void allModelsHaveLeftTheUnit_thenTheReferencesToTheModelsGetNull() {
-		var unit = new Unit();
-		int key1 = unit.add(testEquipment);
-		unit.remove(key1);
-		
-		assertEquals(getWeapons(unit).get(key1), null);
-	}
-	
-	@Test
-	void someModelsJoin_someModelsLeave_KeysStayTheSame() {
-		var unit = new Unit();
-		//The Index starts couting from 0
-		int key1 = unit.add(testEquipment);
-		int key3 = unit.add(testEquipment);
-		unit.remove(key1);
-		unit.remove(key3);
-		int key4 = unit.add(testEquipment);
-		
-		assertEquals(getWeapons(unit).get(0), null);
-		assertEquals(2, key4);
-	}
-	
-	@Test
-	void spaceMarines_against_guardsmen() {
-		//So lets say we have a unit with a simple space marine 
-		//This marine shall attack a guardsman
-		var spaceMarines = new Unit();
-		spaceMarines.add(new Equipment(bolter, 5));
-		double damage = spaceMarines.attack(guardsmen);
-		//10 Attacks with 3+ to hit, 3+ to wound and a 5up save
-		double hits = 10 * Probability.THREE_UP;
-		double wounds = hits * Probability.THREE_UP;
-		double expectedDamage = Math.floor(wounds - (wounds * Probability.FIVE_UP)); 
-		assertEquals(expectedDamage, damage);
-	}
-	
-	@Test
-	void spaceMarines_against_eldarRangers() {
-		//Lets take a test for the invul save
-		//we expect the probability to save to stay constant		
-		var spaceMarines = new Unit();
-		spaceMarines.add(new Equipment(heavyBolter, 4));
-		double damage = spaceMarines.attack(eldarRangers);
-		double hits = 12 * Probability.THREE_UP;
-		double wounds = hits * Probability.THREE_UP;
-		double missedSaves = Math.floor(wounds - (wounds * Probability.FIVE_UP)); 
-		double expectedDamage = missedSaves * eldarRangers.getHitPoints();
-		assertEquals(expectedDamage, damage);
-	}
-	
-	@Test
-	void spaceMarines_against_other_spaceMarines() {
-		var spaceMarines = new Unit();
-		spaceMarines.add(new Equipment(heavyBolter, 4));
-		var otherSpaceMarines = Profile.builder()
-				.toughness(4)
-				.armorSave(Probability.THREE_UP)
-				.hitPoints(2)
-				.build();
-		
-		double damage = spaceMarines.attack(otherSpaceMarines);
-		double hits = 12 * Probability.THREE_UP;
-		double wounds = hits * Probability.THREE_UP;
-		double failedSaves = wounds - (wounds * Probability.FIVE_UP); 
-		//the damage is capped to the maximum of hitpoints on the target
-		//For every full wound (pessimisticly rounding down) we deal damage
-		//The heavy Bolter does 2 damage so we can take only 2 damage 
-		int hitPoints = otherSpaceMarines.getHitPoints();
-		double expectedDamage = Math.floor(failedSaves) * hitPoints;
-		assertEquals(expectedDamage, damage);
-	}
-	
-	@Test
-	void spaceMarines_verus_abberants() {
-		var spaceMarines = new Unit();
-		spaceMarines.add(new Equipment(bolter, 5));
-		var abberants = Profile.builder()
-				.toughness(6)
-				.armorSave(Probability.FIVE_UP)
-				.hitPoints(3)
-				.feelNoPain(Probability.FOUR_UP)
-				.build();
-		
-		double damage = spaceMarines.attack(abberants);
-		double hits = 10 * Probability.THREE_UP;
-		double wounds = hits * Probability.FIVE_UP;
-		double missedSaves = Math.floor(wounds - (wounds * Probability.FIVE_UP));
-		//Feel no pain wound migitation
-		double expectedDamage = missedSaves * Probability.FOUR_UP;
-		assertEquals(expectedDamage, damage);
-	}
-	
-	@Test
-	void SpaceMarinesWithACaptian_AttackGuardsmen_RerollOnesToHit() {
-		var spaceMarines = new Unit();
-		spaceMarines.add(SpecialRuleUnit.REROLL_ONES_TO_HIT);
-		spaceMarines.add(new Equipment(bolter, 5));
-		var damage = spaceMarines.attack(guardsmen);
-		//We implement the reroll
-		var hits = 10 * Probability.THREE_UP;
-		var missedHits = 10 - hits;
-		//We reroll only ones
-		hits += (missedHits / 6) * Probability.THREE_UP;
-		
-		var wounds = hits * Probability.THREE_UP;
-		//but we also redefined the formula for the wound roll
-		var expectedDamage = Math.floor(wounds - (wounds * Probability.FIVE_UP));
-		assertEquals(expectedDamage, damage);
-	}
-	
-	private HashMap<Integer, Equipment> getWeapons(Unit unit) {
-		try {
-			var field = Unit.class.getDeclaredField("weapons");
-	        field.setAccessible(true); 
-	        var attributeValue = (HashMap<Integer, Equipment>) field.get(unit);
-	        field.setAccessible(false);
-	        return attributeValue;
-		} catch (Exception e) {
-			return null;
-		}
-    }
+	Profile abberants = Profile.builder()
+			.toughness(6)
+			.armorSave(Probability.FIVE_UP)
+			.hitPoints(3)
+			.feelNoPain(Probability.FOUR_UP)
+			.build();
 }
