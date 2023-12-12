f3c2be2327b76fde0c87b8ce16bea8d969bca19c
Till Kaiser <art0s@mailbox.org>
2023-12-12 10:14:20 +0100

    refactor(core): added saving throws to new core

---
+++ src/main/java/core/combat/dicePool/SavingThrowDiceRoll.java
@@ -0,0 +1,50 @@
+package core.combat.dicePool;
+
+import java.util.Collections;
+import java.util.HashMap;
+
+import core.Enemy;
+import core.Enemy.SpecialRuleProfile;
+import core.Probability;
+import core.Unit;
+import core.Weapon;
+import core.combat.CombatRules;
+import lombok.val;
+
+public class SavingThrowDiceRoll extends DiceRoll {
+
+	public SavingThrowDiceRoll(Unit unit, Weapon weapon, Enemy enemy, CombatRules combatRules) {
+		super(unit, weapon, enemy, combatRules);
+	}
+
+	public DicePool roll(DicePool dicePool) {
+		val total = dicePool.total();
+		val armourSave = enemy.getArmorSave();
+		val invulSave = enemy.getInvulnerableSave();
+		
+		val cover = enemy.has(SpecialRuleProfile.HAS_COVER) ? 1 : 0;
+		val modifiedArmourSave = (byte) (ARMOR_SAVES.get(armourSave) - weapon.getArmorPenetration() + cover);
+		val armorSaveProbability = modifiedArmourSave / 6f;
+		val mustTakeInvulSave = invulSave > armorSaveProbability;
+		val save = mustTakeInvulSave ? invulSave : armorSaveProbability;
+		
+		val missedSavingThrows = total - (total * save);
+		return new DicePool(total, missedSavingThrows); 
+	}
+	
+	/**
+	 * The armor save characteristica of an enemy unit tells us how
+	 * tanky a unit is. This implementation subtracts the armor penetration value
+	 * from the weapon from the given values, until there is no armor save left
+	 */
+	private static final HashMap<Float,Byte> ARMOR_SAVES = new HashMap<>();
+	static {
+		ARMOR_SAVES.put(Probability.SIX_UP, (byte) 1);
+		ARMOR_SAVES.put(Probability.FIVE_UP, (byte) 2);
+		ARMOR_SAVES.put(Probability.FOUR_UP, (byte) 3);
+		ARMOR_SAVES.put(Probability.THREE_UP, (byte) 4);
+		ARMOR_SAVES.put(Probability.TWO_UP, (byte) 5);
+		Collections.unmodifiableMap(ARMOR_SAVES);
+	}
+
+}
--- src/test/java/core/combat/dicePool/SavingThrowDiceRollTest.java
+++ src/test/java/core/combat/dicePool/SavingThrowDiceRollTest.java
@@ -1,14 +1,87 @@
 package core.combat.dicePool;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class SavingThrowDiceRollTest {
+import core.Enemy;
+import core.Enemy.SpecialRuleProfile;
+import core.Probability;
+import core.Unit;
+import core.Weapon;
+import core.combat.CombatRules;
+import lombok.val;
 
-	@Test @DisplayName("Test saving throws")
-	void test() {
+class SavingThrowDiceRollTest {
+	
+	private float total = 10;
+	private DicePool dicePool;
+	private Unit unit;
+	private CombatRules combatRules;
+	private Enemy enemy;
+	private Weapon weapon;
+	
+	@BeforeEach
+	void setup(){
+		dicePool = mock(DicePool.class);
+		when(dicePool.total()).thenReturn(total);
+		when(dicePool.result()).thenReturn(0f);
+		
+		unit = mock(Unit.class);
+		weapon = mock(Weapon.class);
+		combatRules = mock(CombatRules.class);
+		enemy = mock(Enemy.class);
+	}
+
+	@Test @DisplayName("Test saving throws - normal - without modifier")
+	void testArmorSave() {
+		when(enemy.getArmorSave()).thenReturn(Probability.FIVE_UP);
+		
+		val diceRoll = new SavingThrowDiceRoll(unit, weapon, enemy, combatRules);
+		val resultPool = diceRoll.roll(dicePool);
+		val missedSavingThrows = total - (total * Probability.FIVE_UP);
+		
+		assertEquals(missedSavingThrows, resultPool.result());
+	}
+	
+	@Test @DisplayName("Test saving throws - with mods - no armor save")
+	void testArmorPen() {
+		when(weapon.getArmorPenetration()).thenReturn((byte) 2);
+		when(enemy.getArmorSave()).thenReturn(Probability.FIVE_UP);
+		
+		val diceRoll = new SavingThrowDiceRoll(unit, weapon, enemy, combatRules);
+		val resultPool = diceRoll.roll(dicePool);
+		
+		assertEquals(total, resultPool.result());
+	}
+	
+	@Test @DisplayName("Test saving throws - with mods - invul save")
+	void testInvulSave() {
+		when(weapon.getArmorPenetration()).thenReturn((byte) 3);
+		when(enemy.getInvulnerableSave()).thenReturn(Probability.FIVE_UP);
+		when(enemy.getArmorSave()).thenReturn(Probability.FIVE_UP);
+		
+		val diceRoll = new SavingThrowDiceRoll(unit, weapon, enemy, combatRules);
+		val resultPool = diceRoll.roll(dicePool);
+		val missedSavingThrows = total - (total * Probability.FIVE_UP);
+		
+		assertEquals(missedSavingThrows, resultPool.result());
+	}
+	
+	@Test @DisplayName("Test saving throws - with mods - no invul - cover")
+	void testCover() {
+		when(enemy.getArmorSave()).thenReturn(Probability.FIVE_UP);
+		when(enemy.has(SpecialRuleProfile.HAS_COVER)).thenReturn(true);
+		
+		val diceRoll = new SavingThrowDiceRoll(unit, weapon, enemy, combatRules);
+		val resultPool = diceRoll.roll(dicePool);
+		val missedSavingThrows = total - (total * Probability.FOUR_UP);
+		
+		assertEquals(missedSavingThrows, resultPool.result());
 	}
 
 }
