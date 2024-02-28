10ab9e7bef287729e4c66d6da89fea7adc373ba5
Till Kaiser <art0s@mailbox.org>
2024-02-29 00:45:53 +0100

    refactor(gui): broken init methods into smaller components

--- src/main/java/unit/UnitView.java
+++ src/main/java/unit/UnitView.java
@@ -63,6 +63,8 @@ public class UnitView implements View {
 	//"Recycled" Widgets - you mostly have to use draw() before using these
 	private Composite compositeUnitEditor;
 	private Composite compositeUnits;
+	private Composite compositeUnitList;
+	private Group unitEditorGroup;
 	
 	public UnitView(TabFolder mainTab) {
 		this.mainTab = mainTab;
@@ -72,93 +74,31 @@ public class UnitView implements View {
 	@Override
 	public void draw() {
 		_initalizeGeneralView();
-		_initializeListView();
+		_initializeMenuBar();
+		_initializeUnitList();
 		_initalizeEditorView();
+		_initializeCheckBoxes();
 	}
 	
-	private void _initalizeGeneralView() { 
-		unitTab = new TabItem(mainTab, SWT.NONE);
-		unitTab.setText(TAB_NAME);
-		
-		compositeUnits = new Composite(mainTab, SWT.NONE);
-		unitTab.setControl(compositeUnits);
-		compositeUnits.setLayout(new GridLayout(1, true));
-		compositeUnits.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		compositeUnits.setLayout(new GridLayout(12, true));	
-	}
-	
-	private void _initializeListView() {
-		Composite compositeUnitList = new Composite(compositeUnits, SWT.NONE);
-		GridData gridDataCompositeUnitList = new GridData(SWT.FILL, SWT.FILL, true, true);
-		gridDataCompositeUnitList.horizontalSpan = 4;
-		compositeUnitList.setLayoutData(gridDataCompositeUnitList);
-		compositeUnitList.setLayout(new GridLayout(1, true));
-		
-		CoolBar menuBar = new CoolBar(compositeUnitList, SWT.NONE);
-		GridData menuBarGridData = new GridData(SWT.FILL, SWT.FILL, false, false);
-		menuBarGridData.verticalSpan = 8;
-		menuBar.setLayoutData(menuBarGridData);
-		menuBar.setLayout(new GridLayout(2, true));
-		addButton = new Button(menuBar, SWT.PUSH);
-		addButton.setImage(imageServer.createImageForButton("plus"));
-		addButton.setToolTipText(ADD);
-		deleteButton = new Button(menuBar, SWT.PUSH);
-		deleteButton.setImage(imageServer.createImageForButton("trash-can"));
-		deleteButton.setToolTipText(DELETE);
-
-		Group unitListGroup = new Group(compositeUnitList, SWT.VERTICAL); 
-		unitListGroup.setText(LIST_VIEW);
-		unitListGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		unitListGroup.setLayout(new GridLayout(1, true));
-		
-		Composite unitListComposite = new Composite(unitListGroup, SWT.NONE);
-		unitListComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		unitListComposite.setLayout(new GridLayout(1, true));
-
-		selectionList = new List(unitListComposite, SWT.NONE);
-		GridData listGridData = new GridData(SWT.FILL, SWT.FILL, true, true);
-		selectionList.setLayoutData(listGridData);
-	}
-	
-	private void _initalizeEditorView() {
-		compositeUnitEditor = new Composite(compositeUnits, SWT.NONE);
-		GridData gridDataCompositeUnitEditor = new GridData(SWT.FILL, SWT.FILL, true, true);
-		gridDataCompositeUnitEditor.horizontalSpan = 8;
-		compositeUnitEditor.setLayoutData(gridDataCompositeUnitEditor);
-		compositeUnitEditor.setLayout(new GridLayout(1, true));
-		
-		Group unitEditorGroup = new Group(compositeUnitEditor, SWT.NONE);
-		unitEditorGroup.setText(GROUP_NAME);
-		unitEditorGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		unitEditorGroup.setLayout(new GridLayout(4, true));
-		
-		Label nameLabel = new Label(unitEditorGroup, SWT.NONE);
-		nameLabel.setText(UNIT_NAME); 
-		nameInput = new Text(unitEditorGroup, SWT.NONE);
-		GridData nameInputGridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
-		nameInputGridData.horizontalSpan = 3;
-		nameInput.setLayoutData(nameInputGridData);
-		
-		Label placeholder = new Label(unitEditorGroup, SWT.NONE);
-		GridData placeholderGridData = new GridData(SWT.FILL, SWT.FILL, true, false);
-		placeholderGridData.horizontalSpan = 4;
-		placeholder.setLayoutData(placeholderGridData);
-		
-		ButtonFactory buttonFactory = new ButtonFactory(unitEditorGroup);
-		checkBoxAddOneToHit = buttonFactory.createCheckBox(ADD_ONE_TO_HIT);
-		checkBoxLethalHits = buttonFactory.createCheckBox(HAS_LETHAL_HITS);
-		checkBoxRerollOnesToHit = buttonFactory.createCheckBox(REROLL_ONES_TO_HIT);
-		checkBoxRerollHitRoll = buttonFactory.createCheckBox(REROLL_HIT_ROLL);
-		
-		checkBoxAddOneToWound = buttonFactory.createCheckBox(ADD_ONE_TO_WOUND);
-		checkBoxRerollOnesToWound = buttonFactory.createCheckBox(REROLL_ONES_TO_WOUND);
-		checkBoxRerollWound = buttonFactory.createCheckBox(REROLL_WOUND_ROLL);
-		checkBoxIgnoreCover = buttonFactory.createCheckBox(IGNORE_COVER);
-	}
-	
-	
-
 	@Override
+	public void drawList(ModelList modelList) { 
+		UnitList unitList = (UnitList) modelList;
+		selectionList.removeAll();
+		
+		val unitListIsEmpty = modelList == null;
+		if(unitListIsEmpty) {
+			return;
+		}
+		
+		for(Unit unit: unitList.getUnits()) {
+			val unitHasNoName = unit.getName() == null;
+			selectionList.add(unitHasNoName ? "" : unit.getName());
+		}
+		
+		selectionList.setSelection(0);
+	}
+	
+	@Override 
 	public void drawEditor(Model model) { 
 		Unit unit = (Unit) model; 
 		val formIsInitialized = model == null;
@@ -180,23 +120,90 @@ public class UnitView implements View {
 		checkBoxRerollWound.setSelection(unit.has(SpecialRuleUnit.REROLL_WOUND_ROLL));
 		checkBoxIgnoreCover.setSelection(unit.has(SpecialRuleUnit.IGNORE_COVER));
 	}
-
-	@Override
-	public void drawList(ModelList modelList) { 
-		UnitList unitList = (UnitList) modelList;
-		selectionList.removeAll();
+	
+	private void _initalizeGeneralView() { 
+		unitTab = new TabItem(mainTab, SWT.NONE);
+		unitTab.setText(TAB_NAME);
 		
-		val unitListIsEmpty = modelList == null;
-		if(unitListIsEmpty) {
-			return;
-		}
+		compositeUnits = new Composite(mainTab, SWT.NONE);
+		unitTab.setControl(compositeUnits);
+		compositeUnits.setLayout(new GridLayout(1, true));
+		compositeUnits.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		compositeUnits.setLayout(new GridLayout(12, true));	
 		
-		for(Unit unit: unitList.getUnits()) {
-			val unitHasNoName = unit.getName() == null;
-			selectionList.add(unitHasNoName ? "" : unit.getName());
-		}
-		
-		selectionList.setSelection(0);
+		compositeUnitList = new Composite(compositeUnits, SWT.NONE);
+		GridData gridDataCompositeUnitList = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gridDataCompositeUnitList.horizontalSpan = 4;
+		compositeUnitList.setLayoutData(gridDataCompositeUnitList);
+		compositeUnitList.setLayout(new GridLayout(1, true));
 	}
 
+	private void _initializeUnitList() {
+		Group unitListGroup = new Group(compositeUnitList, SWT.VERTICAL); 
+		unitListGroup.setText(LIST_VIEW);
+		unitListGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		unitListGroup.setLayout(new GridLayout(1, true));
+		
+		Composite unitListComposite = new Composite(unitListGroup, SWT.NONE);
+		unitListComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		unitListComposite.setLayout(new GridLayout(1, true));
+
+		selectionList = new List(unitListComposite, SWT.NONE);
+		GridData listGridData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		selectionList.setLayoutData(listGridData);
+	}
+
+	private void _initializeMenuBar() {
+		CoolBar menuBar = new CoolBar(compositeUnitList, SWT.NONE);
+		GridData menuBarGridData = new GridData(SWT.FILL, SWT.FILL, false, false);
+		menuBarGridData.verticalSpan = 8;
+		menuBar.setLayoutData(menuBarGridData);
+		menuBar.setLayout(new GridLayout(2, true));
+		
+		addButton = new Button(menuBar, SWT.PUSH);
+		addButton.setImage(imageServer.createImageForButton("plus"));
+		addButton.setToolTipText(ADD);
+		
+		deleteButton = new Button(menuBar, SWT.PUSH);
+		deleteButton.setImage(imageServer.createImageForButton("trash-can"));
+		deleteButton.setToolTipText(DELETE);
+	}
+	
+	private void _initalizeEditorView() { 
+		compositeUnitEditor = new Composite(compositeUnits, SWT.NONE);
+		GridData gridDataCompositeUnitEditor = new GridData(SWT.FILL, SWT.FILL, true, true);
+		gridDataCompositeUnitEditor.horizontalSpan = 8;
+		compositeUnitEditor.setLayoutData(gridDataCompositeUnitEditor);
+		compositeUnitEditor.setLayout(new GridLayout(1, true));
+		
+		unitEditorGroup = new Group(compositeUnitEditor, SWT.NONE);
+		unitEditorGroup.setText(GROUP_NAME);
+		unitEditorGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		unitEditorGroup.setLayout(new GridLayout(4, true));
+		
+		Label nameLabel = new Label(unitEditorGroup, SWT.NONE);
+		nameLabel.setText(UNIT_NAME); 
+		nameInput = new Text(unitEditorGroup, SWT.NONE);
+		GridData nameInputGridData = new GridData(SWT.FILL, SWT.CENTER, true, false);
+		nameInputGridData.horizontalSpan = 3;
+		nameInput.setLayoutData(nameInputGridData);
+		
+		Label placeholder = new Label(unitEditorGroup, SWT.NONE);
+		GridData placeholderGridData = new GridData(SWT.FILL, SWT.FILL, true, false);
+		placeholderGridData.horizontalSpan = 4;
+		placeholder.setLayoutData(placeholderGridData);
+	}
+
+	private void _initializeCheckBoxes() {
+		ButtonFactory buttonFactory = new ButtonFactory(unitEditorGroup);
+		checkBoxAddOneToHit = buttonFactory.createCheckBox(ADD_ONE_TO_HIT);
+		checkBoxLethalHits = buttonFactory.createCheckBox(HAS_LETHAL_HITS);
+		checkBoxRerollOnesToHit = buttonFactory.createCheckBox(REROLL_ONES_TO_HIT);
+		checkBoxRerollHitRoll = buttonFactory.createCheckBox(REROLL_HIT_ROLL);
+		
+		checkBoxAddOneToWound = buttonFactory.createCheckBox(ADD_ONE_TO_WOUND);
+		checkBoxRerollOnesToWound = buttonFactory.createCheckBox(REROLL_ONES_TO_WOUND);
+		checkBoxRerollWound = buttonFactory.createCheckBox(REROLL_WOUND_ROLL);
+		checkBoxIgnoreCover = buttonFactory.createCheckBox(IGNORE_COVER);
+	}
 }
